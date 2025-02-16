9274b801bde7c5cbbf5256a1080fd81673b7a192
bekreth <bekreth@proton.me>
2025-02-15 20:35:54 -0800

    Setup cmocka test

---
+++ lab_05/c/test/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "factorize.c"
+
+int main() {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(null_test_success),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
